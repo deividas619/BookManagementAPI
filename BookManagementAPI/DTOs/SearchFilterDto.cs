8fee31eded3ffe73ec8db7aa941346698fa8b980
Artur <Artur@email.com>
2024-04-25 19:51:08 +0300

    Add Filter DTO

---
+++ BookManagementAPI/DTOs/SearchFilterDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookManagementAPI.DTOs;
+
+public class SearchFilterDto
+{
+	public string? Title { get; set; }
+	public string? Author { get; set; }
+	public string[] Genres { get; set; }
+	public DateOnly? PublicationFromDate { get; set; }
+	public DateOnly? PublicationToDate { get; set; }
+}
